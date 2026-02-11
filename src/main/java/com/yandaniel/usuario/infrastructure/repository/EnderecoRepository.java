e05f21c1cb36c915fcb6927223c471f8e849a27d
Yan <eng.ydsouza@gmail.com>
2026-02-10 22:50:00 -0300

    adicionadas as classes de security

---
+++ src/main/java/com/yandaniel/usuario/infrastructure/repository/EnderecoRepository.java
@@ -0,0 +1,9 @@
+package com.yandaniel.usuario.infrastructure.repository;
+
+import com.yandaniel.aprendendospring.infrastructure.entity.Endereco;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EnderecoRepository extends JpaRepository<Endereco, Long> {
+}
